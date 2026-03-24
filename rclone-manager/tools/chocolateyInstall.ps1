5837e647b152e4ed8a76e0ac4618393cb84c6ae8
Hakan İSMAİL <hakanismail53@gmail.com>
2026-03-24 10:56:26 +0300

    Bump rclone-manager version to 0.2.3 and update installation URLs and checksums

--- rclone-manager/tools/chocolateyInstall.ps1
+++ rclone-manager/tools/chocolateyInstall.ps1
@@ -11,11 +11,11 @@ $packageArgs = @{
 $procArch = Get-ProcessorBits
 
 if ($procArch -eq 64) {
-  $packageArgs.url64bit = 'https://github.com/Zarestia-Dev/rclone-manager/releases/download/v0.2.2/RClone.Manager_0.2.2_x64_en-US.msi'
-  $packageArgs.checksum64 = 'D16D726C3C992ED3AD61E89D0C9FEA36D2A7822A4B55CF402E798E02D032DB9C'
+  $packageArgs.url64bit = 'https://github.com/Zarestia-Dev/rclone-manager/releases/download/v0.2.3/RClone.Manager_0.2.3_x64_en-US.msi'
+  $packageArgs.checksum64 = '9130C2628DE666267D16FEFE373F3053C73DFDF579FE355F9149949EA541D0AB'
 } elseif ($procArch -eq 'arm64') {
-  $packageArgs.url64bit = 'https://github.com/Zarestia-Dev/rclone-manager/releases/download/v0.2.2/RClone.Manager_0.2.2_arm64_en-US.msi'
-  $packageArgs.checksum64 = '120E165413AAA9E134250E0F69001DD5783A39A9965BF1EECD4080276811577F'
+  $packageArgs.url64bit = 'https://github.com/Zarestia-Dev/rclone-manager/releases/download/v0.2.3/RClone.Manager_0.2.3_arm64_en-US.msi'
+  $packageArgs.checksum64 = '2ED17B96CEBA1DF9958FBEB727D761A0ED9148BB7ACC7D5E77CA617E5E697316'
 } else {
   throw "This package does not support the $($procArch) architecture."
 }
